dc969d872e72244d87217c1d7bccfce6c2c38c47
LikAnda <mimipandaaugustin@hotmail.com>
2022-03-13 16:06:55 +0100

    V.1.0.1 (add documention)

--- README.md
+++ README.md
@@ -5,19 +5,25 @@ A small things vending machine with a raspberry.
 
 ## Installation
 
-- `sudo npm install`
+- `npm install` *(to install all dependencies)*
 
-- Motion sensor on pin 11 (GPIO 17)
+- Servo motor on pin 11 (GPIO 17)
 
 - Piezo connected to pin 13 (GPIO 27)
+
+<p align="center">
+<img width="475", height="472" src="https://cdn.discordapp.com/attachments/719932115029852213/952576559372439632/vending-machine_LikAnda.png">
+</p>
 
 ----
 
 ## Run
 
-- `sudo node index.js`
+1. `sudo node index.js`
 
-- To activate the system remotely, go to `ip-of-the-raspberry:2000`
+2. To activate the system remotely, go to `ip-of-the-raspberry:2000`
+
+3. And you just have to activate it from here
 
 ----
 
--- index.js
+++ index.js
@@ -1,75 +1,89 @@
-const five = require('johnny-five')
+const { Board, Servo, Piezo } = require('johnny-five')
 const Raspi = require('raspi-io').RaspiIO
-const board = new five.Board({
+const board = new Board({
   io: new Raspi(),
-})
+}) 
 
 const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
 
-server.listen(2000, () => {
-  console.log('listening on *:2000')
-})
+server.listen(2000, () => {})
+
+let interfaces = require('os').networkInterfaces();
+for (let k in interfaces) {
+    for (let k2 in interfaces[k]) {
+        let address = interfaces[k][k2];
+        if (address.family === 'IPv4' && address.address !== '127.0.0.1' && !address.internal){
+            console.log(`Go on : http://${address.address}:2000 to start the vending machine`);
+        }
+    }
+}
 
 const { Server } = require('socket.io')
 const io = new Server(server)
 
-let runStatus = 'waiting'
-
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-io.on('connection', (socket) => {
-  console.log('A user is ready to activate the vending machine')
-})
-
-socket.on('start-running', function (vending) {
-  if (runStatus === 'waiting') {
-    runStatus = 'running'
-  }
-})
+let servo = null;
+let piezo = null;
 
 board.on('ready', function () {
-
-  const servo = new Servo({
-    pin: 'P1-15',
+  servo = new Servo({
+    pin: 'P1-11',
     type: 'continuous',
     pwmRange: [600, 2900],
   })
-  const piezo = new five.Piezo('P1-13')
+  piezo = new Piezo('P1-13')
   board.repl.inject({
     piezo: piezo,
   })
-
-  if (runStatus === 'running') {
-    
-    servo.cw()
-    piezo.play({
-      song: [
-        ['C4', 1 / 4],
-        ['D4', 1 / 4],
-        ['F4', 1 / 4],
-      ],
-      tempo: 100, 
-    })
-
-    // attendre 3 secondes
-
-    servo.ccw()
-    piezo.play({
-      song: [
-        ['C4', 1 / 4],
-        ['D4', 1 / 4],
-        ['F4', 1 / 4],
-      ],
-      tempo: 100, 
-    })
-    runStatus = 'waiting'
-
-  } else if (runStatus === 'waiting') {
-      console.log('An error has occurred')
-  }
 })
+
+io.on('connection', (socket) => {
+
+  socket.emit('state-change', { state: 'waiting' })
+
+  console.log('A user is ready to activate the vending machine')
+
+  socket.on('start-running', () => {
+
+      socket.emit('state-change', { state: 'running' })
+
+      console.log('Servo open')
+
+      servo.cw()
+      piezo.play({
+        song: [
+          ['C4', 1 / 4],
+          ['D4', 1 / 4],
+          ['F4', 1 / 4],
+        ],
+        tempo: 100,
+      })
+
+      const myInterval = setInterval(function () {
+        console.log('Servo closed')
+
+        servo.ccw()
+        piezo.play({
+          song: [
+            ['C4', 1 / 4],
+            ['D4', 1 / 4],
+            ['F4', 1 / 4],
+          ],
+          tempo: 100,
+        })
+        
+        socket.emit('state-change', { state: 'waiting' })
+
+        clearInterval(myInterval)
+      }, 2500)
+    })
+})
+
+
+
